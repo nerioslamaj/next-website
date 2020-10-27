a784370eae015a546f88caa5c9975ba58babb8c8
Nerios Lamaj <nerios.lamaj@gmail.com>
2020-10-27 13:34:59 +0000

    Updating the preview and delivery tokens

--- utils/contentfulPosts.js
+++ utils/contentfulPosts.js
@@ -1,5 +1,7 @@
 const space = process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID
-const accessToken = process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN
+const accessToken = process.env.NEXT_PUBLIC_CONTENTFUL_BRANCH === 'staging'
+                    ? process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN_PREVIEW
+                    : process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN
 
 const client = require('contentful').createClient({
   space: space,
