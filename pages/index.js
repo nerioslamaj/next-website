643cf33abb427405bd402b1122ada40bc29a07d0
Nerios Lamaj <nerios.lamaj@gmail.com>
2020-10-29 11:46:51 +0000

    Arranging the head tag

--- pages/index.js
+++ pages/index.js
@@ -8,9 +8,19 @@ import Footer from '../components/Footer/Footer'
 export default function Home({ menuItems }) {
   return (
     <div className="container">
-      <Head>
-        <title>Next + Contentful Starter</title>
-        <link rel="icon" href="/favicon.ico" />
+      <Head>    <meta charset="utf-8" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="theme-color" content="#000000" />
+        <meta property="og:title" content="Nerios Lamaj - Portfolio Website" />
+        <meta property="og:description" content="Nerios Lamaj - Portfolio Website" />
+        <meta property="og:image" content="https://nerios.dev/thumbnail.jpg" />
+        <meta property="og:url" content="https://nerios.dev" />
+        <meta property="og:site_name" content="Nerios Lamaj - Portfolio Website" />
+        <meta name="twitter:image:alt" content="Nerios Lamaj - Portfolio Website" />
+        <meta name="twitter:site" content="@nerioslamaj"/>
+        <link rel="manifest" href="/manifest.json" />
+        <title>Nerios Lamaj - Personal Portfolio</title>
       </Head>
 
       <main>
