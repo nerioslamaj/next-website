12c2a3d69aa370282227c2c6aca59df2857b06fb
Nerios Lamaj <nerios.lamaj@gmail.com>
2020-10-29 11:40:41 +0000

    Updating the css

--- pages/index.js
+++ pages/index.js
@@ -14,47 +14,10 @@ export default function Home({ menuItems }) {
       </Head>
 
       <main>
-        <Header />
+        <Header/>
       </main>
 
-      <Footer />
-
-      <style jsx>{`
-        .container {
-          height: 100vh;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        .posts {
-          display: flex;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu,
-            Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
+      <Footer/>
     </div>
   )
 }
