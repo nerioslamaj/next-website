8c20bbc96e42ea95e0052869d336cc41ab789be3
Nerios Lamaj <nerios.lamaj@gmail.com>
2020-11-02 11:56:17 +0000

    Adding a Canvas shader to homepage

---
+++ components/Shader/Shader.spec.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { createContainer } from '../_fixtures/domManipulators'
+import { Shader } from './Shader'
+
+describe('Shader Component', () => {
+    let render, container
+
+    beforeEach(() => {
+        ({ render, container } = createContainer())
+    });
+
+    it('renders the canvas shader parent', () => {
+        render(<Shader />)
+
+        expect(container.querySelector('#shaderParent')).not.toBeNull()
+    });
+});
