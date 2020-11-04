2e5d070de34aba75179084135f66e814e72c006d
Nerios Lamaj <nerios.lamaj@gmail.com>
2020-11-04 12:58:35 +0000

    Updating styles path

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import '../global/_app.scss'
+import '../styles/_app.scss'
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
