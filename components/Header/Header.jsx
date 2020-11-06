9b4362652841a1d4634b54ba07b2db42db5fd25f
Nerios Lamaj <nerios.lamaj@gmail.com>
2020-11-06 13:24:24 +0000

    Refining Header Styles

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,19 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './Header.scss'
 
 export const Header = ({ logo, items }) => {
+
+  const handleMenuClick = page => {
+    console.log(page)
+  }
+
   return (
-    <header>
-      <img src={logo.file.url} />
-      <ul>
-        {items.map((item) => {
-          return (
-            <li key={item.toLowerCase()}>
-              <a href={item.toLowerCase()}>{item}</a>
-            </li>
-          )
-        })}
-      </ul>
+    <header className="NL-Row ">
+      <div className="NL-Container inner-menu">
+        <img className="Logo" src={logo.file.url} />
+        <ul>
+          {items.map((item) => {
+            return (
+              <li key={item.toLowerCase()}>
+                <a href={item.toLowerCase()} onClick={handleMenuClick(item.toLowerCase())}>{item}</a>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
     </header>
   )
 }
