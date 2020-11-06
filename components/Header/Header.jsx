e9958e2199e6d0e0d01c74982ca4800b8cb65cd6
Nerios Lamaj <nerios.lamaj@gmail.com>
2020-11-06 14:37:44 +0000

    Restyling Header

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -8,14 +8,14 @@ export const Header = ({ logo, items }) => {
   }
 
   return (
-    <header className="NL-Row ">
-      <div className="NL-Container inner-menu">
+    <header className="NL-Row">
+      <div className="NL-Container Inner-Menu">
         <img className="Logo" src={logo.file.url} />
         <ul>
           {items.map((item) => {
             return (
               <li key={item.toLowerCase()}>
-                <a href={item.toLowerCase()} onClick={handleMenuClick(item.toLowerCase())}>{item}</a>
+                <a className="NL-No-Txt-Dec" href={item.toLowerCase()} onClick={handleMenuClick(item.toLowerCase())}>{item}</a>
               </li>
             )
           })}
